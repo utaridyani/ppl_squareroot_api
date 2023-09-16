ba8d2b24f453f4822830a7b825757f0cf5b21bd1
utaridyani <dyanilaksmi28@gmail.com>
2023-09-16 08:58:29 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/helloworld', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.get('/square-root/:number', (req, res) => {
+    const number = parseFloat(req.params.number);
+  
+    // For 0 and 1, the square roots are themselves
+    if (number < 2) {
+      return res.json({ result: number });
+    }
+  
+    // Calculate the square root
+    let y = number;
+    let z = (y + number / y) / 2;
+  
+    // As we want to get up to 5 decimal digits, the absolute
+    // difference should not exceed 0.00001
+    while (Math.abs(y - z) >= 0.00001) {
+      y = z;
+      z = (y + number / y) / 2;
+    }
+  
+    return res.json({ result: parseFloat(z.toFixed(5)) });
+  });
+
+app.listen(port, () => {
+  console.log(`cli-nodejs-api listening at http://localhost:${port}`)
+});
